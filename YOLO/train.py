1a392c0a28079efc410a941b5699f7c770fd6dd8
anesh1234 <112165922+anesh1234@users.noreply.github.com>
2025-03-18 15:41:44 +0100

    yolo work continued

--- YOLO/train.py
+++ YOLO/train.py
@@ -1,10 +1,13 @@
+from gc import freeze
 from ultralytics import YOLO
 import torch
 import multiprocessing
-from pathlib import Path
-import os
+import yaml
 
 def main():
+    
+    
+    
     # Show GPU status
     DEVICE = None
     if torch.cuda.is_available():
@@ -13,21 +16,29 @@ def main():
     else:
         print('\nTorch can Access CUDA: FALSE\n')
         DEVICE = 'cpu'
+    
+    # Load a configuration YAML file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
     # Load the YOLOv11 nano pretrained model if it exists,
     # downloads otherwise
     model = YOLO("yolo11n.pt")
 
+    for k, v in model.named_parameters():
+        print(k)
+
     # Train the model
     results = model.train(data= "datasets/d1/data.yaml",  # data (str): Path to dataset configuration file.
-                        epochs= 5,                       # epochs (int): Number of training epochs., 
+                        epochs= 5,                        # epochs (int): Number of training epochs. 
                         batch= 10,                        # batch (int): Batch size for training.
                         imgsz= 640,                       # imgsz (int): Input image size.
                         device= DEVICE,                   # device (str): Device to run training on (e.g., 'cuda', 'cpu').
-                        optimizer= 'auto',                # optimizer (str): Optimizer to use for training.
+                        optimizer= 'Adam',                # optimizer (str): Options include SGD, Adam, AdamW, NAdam, RAdam, RMSProp etc., or auto for automatic selection based on model configuration. Affects convergence speed and stability.
                         lr0= None,                        # lr0 (float): Initial learning rate.
                         patience= None,                   # patience (int): Epochs to wait for no observable improvement for early stopping of training.
-                        plots= True                       # plots (bool): Generates and saves plots of training and validation metrics, as well as prediction examples, providing visual insights into model performance and learning progression.
+                        plots= True,                      # plots (bool): Generates and saves plots of training and validation metrics, as well as prediction examples, providing visual insights into model performance and learning progression.
+                        freeze= config['freeze']          # freeze (int or list): Freezes the first N layers of the model or specified layers by index, reducing the number of trainable parameters. Useful for fine-tuning or transfer learning.
                         )
 
     # YOLO hyperparameters:
