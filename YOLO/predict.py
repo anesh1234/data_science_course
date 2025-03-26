dcb37203c7510d853d5ddc56e5c7b7331d8e33c3
Anders Minde <anders.minde@gmail.com>
2025-03-26 23:15:39 +0100

    final cleanup

--- YOLO/predict.py
+++ YOLO/predict.py
@@ -1,19 +1,20 @@
 from ultralytics import YOLO
 
 # Load the YOLOv11 nano pretrained model
-model = YOLO("YOLO/test/train4/weights/best.pt")  # pretrained YOLO11n model
+model = YOLO("YOLO/pt_nfrozen/train_id_3_part2/weights/best.pt")  # Our best model
 
 # Run batched inference on a list of images
 # Returns a list of Results objects
-results = model(["datasets/d1/test/images/-1-_bmp.rf.0a1b63b0ef0ec1961342da2ff7908e12.jpg", 
-                 "datasets/d1/test/images/-2-_jpg.rf.d0496bd87fc5593df878655de45dc9ac.jpg"])
+results = model(["datasets/final/valid/images/-63-_jpg.rf.87fd46fd7c7430910483d572be568b07.jpg", 
+                 "datasets/final/valid/images/103_jpg.rf.b8b9120fb6b26e4743068d6d918c5c87.jpg",
+                 "datasets/final/valid/images/0947_jpg.rf.ad5698df1fd26668334dfdf85afff5bf.jpg"])
 
 # Process results list
-for result in results:
+for it, result in enumerate(results):
     boxes = result.boxes  # Boxes object for bounding box outputs
     masks = result.masks  # Masks object for segmentation masks outputs
     keypoints = result.keypoints  # Keypoints object for pose outputs
     probs = result.probs  # Probs object for classification outputs
     obb = result.obb  # Oriented boxes object for OBB outputs
     result.show()  # display to screen
-    result.save(filename="result.jpg")  # save to disk
+    result.save(filename=f"Inference_result_{it}.jpg")  # save to disk
